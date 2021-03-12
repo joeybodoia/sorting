ffb2f1db9178ed4435cb074afb57cc7f31b0d5a5
Joey Bodoia <jbodoia21@cmc.edu>
2021-03-12 15:08:36 -0800

    test cases should be passing

--- sorting.py
+++ sorting.py
@@ -4,13 +4,15 @@ Python provides built-in sort/sorted functions that use timsort internally.
 You cannot use these built-in functions anywhere in this file.
 
 Every function in this file takes a comparator `cmp` as input
-which controls how the elements of the list should be compared against each other:
+which controls how the elements of the list
+should be compared against each other:
 If cmp(a, b) returns -1, then a < b;
 if cmp(a, b) returns  1, then a > b;
 if cmp(a, b) returns  0, then a == b.
 '''
 
 import random
+
 
 def cmp_standard(a, b):
     '''
@@ -64,19 +66,47 @@ def _merged(xs, ys, cmp=cmp_standard):
 
     NOTE:
     In python, helper functions are frequently prepended with the _.
-    This is a signal to users of a library that these functions are for "internal use only",
+    This is a signal to users of a library that these
+    functions are for "internal use only",
     and not part of the "public interface".
 
-    This _merged function could be implemented as a local function within the merge_sorted scope rather than a global function.
-    The downside of this is that the function can then not be tested on its own.
-    Typically, you should only implement a function as a local function if it cannot function on its own
+    This _merged function could be implemented as a local function within
+    the merge_sorted scope rather than a global function.
+    The downside of this is that the function can then not be
+    tested on its own.
+    Typically, you should only implement a function as a local function
+    if it cannot function on its own
     (like the go functions from binary search).
     If it's possible to make a function stand-alone,
-    then you probably should do that and write test cases for the stand-alone function.
+    then you probably should do that and write test cases for
+    the stand-alone function.
 
     >>> _merged([1, 3, 5], [2, 4, 6])
     [1, 2, 3, 4, 5, 6]
     '''
+    left_xs = 0
+    left_ys = 0
+    return_list = []
+    while(left_xs <= len(xs) - 1 and left_ys <= len(ys) - 1):
+        if cmp(xs[left_xs], ys[left_ys]) == -1:
+            return_list.append(xs[left_xs])
+            left_xs += 1
+        elif cmp(xs[left_xs], ys[left_ys]) == 1:
+            return_list.append(ys[left_ys])
+            left_ys += 1
+        else:
+            return_list.append(xs[left_xs])
+            return_list.append(ys[left_ys])
+            left_xs += 1
+            left_ys += 1
+    while (left_xs < len(xs)):
+        return_list.append(xs[left_xs])
+        left_xs += 1
+
+    while (left_ys < len(ys)):
+        return_list.append(ys[left_ys])
+        left_ys += 1
+    return return_list
 
 
 def merge_sorted(xs, cmp=cmp_standard):
@@ -95,6 +125,13 @@ def merge_sorted(xs, cmp=cmp_standard):
     You should return a sorted version of the input list xs.
     You should not modify the input list xs in any way.
     '''
+    if len(xs) <= 1:
+        return xs
+    mid = len(xs) // 2
+    left = merge_sorted(xs[:mid])
+    right = merge_sorted(xs[mid:])
+
+    return _merged(left, right)
 
 
 def quick_sorted(xs, cmp=cmp_standard):
@@ -102,8 +139,9 @@ def quick_sorted(xs, cmp=cmp_standard):
     Quicksort is like mergesort,
     but it uses a different strategy to split the list.
     Instead of splitting the list down the middle,
-    a "pivot" value is randomly selected, 
-    and the list is split into a "less than" sublist and a "greater than" sublist.
+    a "pivot" value is randomly selected,
+    and the list is split into a "less than" sublist
+    and a "greater than" sublist.
 
     The pseudocode is:
 
@@ -120,6 +158,24 @@ def quick_sorted(xs, cmp=cmp_standard):
     You should return a sorted version of the input list xs.
     You should not modify the input list xs in any way.
     '''
+    if len(xs) <= 1:
+        return xs
+    p = random.randint(0, len(xs) - 1)
+    left = []
+    equal = []
+    right = []
+    for i in range(len(xs)):
+        if cmp(xs[i], xs[p]) == -1:
+            left.append(xs[i])
+        elif cmp(xs[i], xs[p]) == 1:
+            right.append(xs[i])
+        else:
+            equal.append(xs[i])
+    left_sort = quick_sorted(left)
+    equal_sort = quick_sorted(equal)
+    right_sort = quick_sorted(right)
+
+    return left_sort + equal_sort + right_sort
 
 
 def quick_sort(xs, cmp=cmp_standard):
@@ -128,16 +184,22 @@ def quick_sort(xs, cmp=cmp_standard):
     The main advantage of quick_sort is that it can be implemented "in-place".
     This means that no extra lists are allocated,
     or that the algorithm uses Theta(1) additional memory.
-    Merge sort, on the other hand, must allocate intermediate lists for the merge step,
+    Merge sort, on the other hand, must allocate intermediate lists for
+    the merge step,
     and has a Theta(n) memory requirement.
-    Even though quick sort and merge sort both have the same Theta(n log n) runtime,
-    this more efficient memory usage typically makes quick sort faster in practice.
+    Even though quick sort and merge sort both have the same
+    Theta(n log n) runtime,
+    this more efficient memory usage typically makes quick sort
+    faster in practice.
     (We say quick sort has a lower "constant factor" in its runtime.)
-    The downside of implementing quick sort in this way is that it will no longer be a [stable sort](https://en.wikipedia.org/wiki/Sorting_algorithm#Stability),
+    The downside of implementing quick sort in this way is that it will
+    no longer be a
+    [stable sort](https://en.wikipedia.org/wiki/Sorting_algorithm#Stability),
     but this is typically inconsequential.
 
     Follow the pseudocode of the Lomuto partition scheme given on wikipedia
     (https://en.wikipedia.org/wiki/Quicksort#Algorithm)
     to implement quick_sort as an in-place algorithm.
-    You should directly modify the input xs variable instead of returning a copy of the list.
+    You should directly modify the input xs variable instead of returning
+    a copy of the list.
     '''
